436b6c55b854d072be238aaafecb68a841e1a2f1
Keegan McAllister <mcallister.keegan@gmail.com>
2017-07-16 15:39:32 -0700

    Warning attributes

--- examples/fib.rs
+++ examples/fib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(feature = "cargo-clippy", allow(print_with_newline))]
+
 extern crate minrisc;
 
 use std::env;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![deny(warnings)]
+
 #[macro_use]
 extern crate enum_primitive;
 extern crate num;
